78d666465c4c9b7287daa3b7c0a474d0ad0c1a05
msujatha <suja_mudupalli@yahoo.com>
2019-01-30 09:48:22 -0800

    Added page for Sujatha

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+
+@app.route('/names/Sujatha/')
+def sujatha():
+    return "Sujatha is working on ToySharing project"
