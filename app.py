9218d3893268a5087b22e071794c2a25c16173c0
Nataly <costasnataly@gmail.com>
2019-01-30 10:11:22 -0800

    added rout with my name

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+
+@app.route('/names/nataly')
+def nataly():
+return "I'm Nataly"
+
