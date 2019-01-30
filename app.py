a6a4debf9103fce4fd4a22cb996b62ce4db19026
Olivia <41270317+phamoh@users.noreply.github.com>
2019-01-30 09:45:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+
+@app.route('names/olivia')
+def olivia()
+    return "I'm Olivia"
