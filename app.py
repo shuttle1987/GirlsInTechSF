52d5c218e53fa68e5f0c12b6bfabc937db25aeb3
puiwei <28238300+puiwei@users.noreply.github.com>
2019-01-30 09:45:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+
+@app.route('/name/kaye')
+def kaye():
+    return "Hi, this is Kaye"
