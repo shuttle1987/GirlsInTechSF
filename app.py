9c40e2b4b2e728bc7f8d26d71e4339bac9c4fe29
Annie Ho <girlybit@gmail.com>
2019-01-30 09:45:46 -0800

    added name/annie endpoint

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+
+@app.route('name/annie')
+def hello():
+    project = "AH"
+    return f"annie is working on Project {project}"
