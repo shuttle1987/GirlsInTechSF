2ade980e32e83dbfb5dd91d124ba2144d625aabc
NLT <NamibiaLT@users.noreply.github.com>
2019-01-30 09:55:51 -0800

    added name Namibia

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+    @app.route('/names/Namibia'):
+     def Namibia():
+         return "Cheers, I'm Namibia!"
