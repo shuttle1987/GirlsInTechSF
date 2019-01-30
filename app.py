d5824ef72a88d069062cb82aa01b59bb5e0d35e6
Ayodele <aro1871@gmail.com>
2019-01-30 09:44:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
+
+@app.route('/names/Ayodele'):
+    def Ayodele():
+        return "I'm Ayodele!"
