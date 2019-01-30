42a41db59408592ed4b021a76048688bdc1b0d9e
Xochitl Soto <40840710+womeat@users.noreply.github.com>
2019-01-30 09:46:33 -0800

    Update app.py
    add an endpoint Xoch

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+@app.route('/names/Xoch')
+def index():
+    return "Xoch"
+
 @app.route('/')
 def index():
     return "Please create an endpoint with your name as the URL"
